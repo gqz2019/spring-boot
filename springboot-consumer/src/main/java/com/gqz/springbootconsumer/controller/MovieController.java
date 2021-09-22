52c9f70ce3615c65e74aaa22c4615fffeb0c1ff4
gqz2019 <gqz2019@outlook.com>
2021-09-22 15:40:33 +0800

    hystrix加入

--- springboot-consumer/src/main/java/com/gqz/springbootconsumer/controller/MovieController.java
+++ springboot-consumer/src/main/java/com/gqz/springbootconsumer/controller/MovieController.java
@@ -1,11 +1,16 @@
 package com.gqz.springbootconsumer.controller;
 
 import com.gqz.springbootconsumer.pojo.User;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * <p></p>
@@ -18,8 +23,16 @@ import org.springframework.web.client.RestTemplate;
 public class MovieController {
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private DiscoveryClient discoveryClient;
     @GetMapping("look")
+    @HystrixCommand(fallbackMethod = "defaultMethod")
     public User look(){
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("provider");
+
+        ServiceInstance serviceInstance = instances.get(0);
+        System.out.println(serviceInstance.getUri());
 
         User user = restTemplate.getForObject("http://localhost:8080/user/221", User.class);
 
